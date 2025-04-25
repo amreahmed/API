391006815d768a367d553a42dcf9e141ae947d9c
John Doe <johndoe@example.com>
2025-04-25 22:20:19 +0300

    ttt

---
+++ models/Planet.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const plantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  defaultTemp: {
+    min: { type: Number, required: true },
+    max: { type: Number, required: true },
+  },
+  defaultLight: {
+    min: { type: Number, required: true },
+    max: { type: Number, required: true },
+  },
+  defaultSoil: {
+    min: { type: Number, required: true },
+    max: { type: Number, required: true },
+  },
+  description: { type: String },
+  photo: { type: String },
+});
+
+
+module.exports = mongoose.model("Plant", plantSchema);
